dce31c3923f7b9679f04c6229cc66a805af5be90
FIAT GORDIN <kellyancurcio@gmail.com>
2025-10-23 20:24:21 -0300

    Update FIAT_HUB.lua
    Kj

--- FIAT_HUB.lua
+++ FIAT_HUB.lua
@@ -1,5 +1,6 @@
 -- Script de auto-load e verificação de jogo
 repeat wait() until game:IsLoaded()
+
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 local placeId = game.PlaceId
@@ -10,8 +11,11 @@ if placeId == 2753915549 then
 elseif placeId == 4924922222 then
     -- FIAT BROKHAVEN Premium
     loadstring(game:HttpGet("https://raw.githubusercontent.com/FIAT23387/FIAT-BROKHAVEN-Premium.lua/main/FIAT%20BROKHAVEN%20Premium.lua", true))()
+elseif placeId == 18687417158 then
+    -- FIAT FORSAKEN
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FIAT23387/Forsaken-script-Fiat.lua/main/Fiat%20forsaken.lua", true))()
 else
     -- Espera um pouco pra garantir carregamento visual antes do kick
     task.wait(1)
-    player:Kick("Esse jogo não suporta o script. Apenas Blox Fruits e BROKHAVEN.")
+    player:Kick("Esse jogo não suporta o script. Apenas Blox Fruits, BROKHAVEN e Forsaken.")
 end
